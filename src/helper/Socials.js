b56282b37caecf2411b70b5edb0716ad040e6920
Shivam Upadhayay <53188725+shiv159@users.noreply.github.com>
2023-03-21 16:58:16 +0530

    Update Socials.js

--- src/helper/Socials.js
+++ src/helper/Socials.js
@@ -1,4 +1,4 @@
-export const resumeLink = 'https://drive.google.com/file/d/185G3WA9obt9CJjadW6h_jdLi-cukEwaj/view?usp=sharing';
+export const resumeLink = 'https://drive.google.com/uc?export=download&id=185G3WA9obt9CJjadW6h_jdLi-cukEwaj';
 export const github = 'https://github.com/shiv159';
 export const linkedin = 'https://www.linkedin.com/in/shivam159';
 export const twitter = 'https://twitter.com/ShivamU81637759';
